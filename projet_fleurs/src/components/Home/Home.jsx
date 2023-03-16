6d60224007917c305c5f7ee33748960eaf0a7dc7
huor97 <huor97ka@gmail.com>
2023-03-16 10:33:18 +0100

    ajouter page principale avec exemple articl

--- projet_fleurs/src/components/Home/Home.jsx
+++ projet_fleurs/src/components/Home/Home.jsx
@@ -1,9 +1,53 @@
 import React from "react";
+import Articl from "./Articl/Articl";
+const products = [
+	{
+	nom:"Bouquet fête des grands-mères",
+	img:"https://kokomo.fr/wp-content/uploads/2023/02/groupon-1-scaled.jpg.webp",
+	prix:"70€"
+},
+	{
+	nom:"Bouquet fête des grands-mères",
+	img:"https://kokomo.fr/wp-content/uploads/2023/02/groupon-1-scaled.jpg.webp",
+	prix:"70€"
+},
+	{
+	nom:"Bouquet fête des grands-mères",
+	img:"https://kokomo.fr/wp-content/uploads/2023/02/groupon-1-scaled.jpg.webp",
+	prix:"70€"
+},
+	{
+	nom:"Bouquet fête des grands-mères",
+	img:"https://kokomo.fr/wp-content/uploads/2023/02/groupon-1-scaled.jpg.webp",
+	prix:"70€"
+},
+	{
+	nom:"Bouquet fête des grands-mères",
+	img:"https://kokomo.fr/wp-content/uploads/2023/02/groupon-1-scaled.jpg.webp",
+	prix:"70€"
+},
+	{
+	nom:"Bouquet fête des grands-mères",
+	img:"https://kokomo.fr/wp-content/uploads/2023/02/groupon-1-scaled.jpg.webp",
+	prix:"70€"
+}
+]
+
 
 const Home = () =>{
     return(
         <>
-         c'est home page
+         <div className="w-full mt-6 pl-[130px] pr-[130px] justify-items-center  grid grid-cols-3 gap-x-8">
+		{products.map((produict, index)=>(
+	            <Articl 
+			key = {index}
+			img = {produict.img}
+			nom = {produict.nom}
+			prix = {produict.prix}
+			
+		    />
+	        ))}
+	    </div>
         </>
 
     )
