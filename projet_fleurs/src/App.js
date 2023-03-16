8d918a54da8e0c53e8cdae2c7aa1aabf4d3a8c76
Nastiakor <a.korotkova@yahoo.fr>
2023-03-16 10:25:24 +0100

    Navbar

--- projet_fleurs/src/App.js
+++ projet_fleurs/src/App.js
@@ -1,7 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header";
-import Login from "./components/Login";
 import Navbar from "./components/Navbar";
 
 function App() {
