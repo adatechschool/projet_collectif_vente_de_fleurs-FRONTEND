fc3d6b15ec40f3005e63145b93abd1bb8500cbc9
Marie Koscianski-Ducharlet <115166678+MarieKosDuc@users.noreply.github.com>
2023-03-16 10:24:38 +0100

    Pages inscription et produit première version

---
+++ projet_fleurs/src/components/Inscription/InscriptionPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "./Header";
+import SignupForms from "./Inscription-components/InscriptionForms"
+
+const InscriptionPage = () => {
+    return <div>
+        <SignupForms />
+    </div>
+}
+
+export default InscriptionPage;
