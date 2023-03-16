bd9fb99979172a7d14255038d88bd104dbb02176
Nastiakor <a.korotkova@yahoo.fr>
2023-03-16 09:51:25 +0100

    Navbar

--- projet_fleurs/src/components/Navbar.jsx
+++ projet_fleurs/src/components/Navbar.jsx
@@ -9,8 +9,10 @@ const Navbar = () => {
     setNav(!nav);
   };
   return (
-    <div className="flex justify-between items-center h-22 max-w-[1200 px] mx-auto px-4 text-black">
-      <h1 className="w-full text-3xl font-bold text-[#aae6d3]">Flowers</h1>
+    <div className="flex justify-between items-center h-24 max-w-[1240 px] mx-auto px-4 text-black">
+      <h1 className="w-full text-2xl font-bold text-[#8be2c6]">
+        Girls & Rouh's flowers
+      </h1>
       <ul className="flex">
         <li className="p-8">Home</li>
         <li className="p-8">Bouquets</li>
@@ -18,18 +20,22 @@ const Navbar = () => {
         <li className="p-8">Cadeaux</li>
         <li className="p-8">Contact</li>
       </ul>
+
+      {/* When we click, if nav is not true, show Close else Menu */}
       <div onClick={handleNav}>
         {!nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
       </div>
-      {/* show menu when clicked  */}
+      {/* show menu when clicked else hidden */}
       <div
         className={
           !nav
-            ? "fixed left-0 top-0 w-[20%] h-full border-r border-r-gray-900 bg-[#acc2ac] ease-in-out duration 500"
+            ? "fixed left-0 top-0 w-[20%] h-full border-r border-r-gray-900 bg-[#b4d6b4] ease-in-out duration 500"
             : "fixed left-[-100%]"
         }
       >
-        <ul className="uppercase p-4">
+        <ul className="pt-24 uppercase p-4">
+          {" "}
+          {/* pt padding top  */}
           <li className="p-4">Home</li>
           <li className="p-4">Bouquets</li>
           <li className="p-4">Bonnes affaires</li>
