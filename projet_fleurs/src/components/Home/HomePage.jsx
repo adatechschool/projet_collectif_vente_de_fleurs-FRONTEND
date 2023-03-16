ac61e035a23a0d65640bf848a117ddc288aa6170
huor97 <huor97ka@gmail.com>
2023-03-16 15:45:32 +0100

    affiché chaque produit dans page articl en clickant sur une produit avec son id articl dans page home

--- projet_fleurs/src/components/Home/HomePage.jsx
+++ projet_fleurs/src/components/Home/HomePage.jsx
@@ -2,31 +2,37 @@ import React from "react";
 import Articl from "./Articl/Articl";
 const products = [
 	{
+	id:"1",
 	nom:"Bouquet fête des grands-mères",
 	img:"https://kokomo.fr/wp-content/uploads/2023/02/groupon-1-scaled.jpg.webp",
 	prix:"70€"
 },
 	{
+	id:"2",
 	nom:"Bouquet fête des grands-mères",
 	img:"https://kokomo.fr/wp-content/uploads/2023/02/groupon-1-scaled.jpg.webp",
 	prix:"70€"
 },
 	{
+	id:"3",
 	nom:"Bouquet fête des grands-mères",
 	img:"https://kokomo.fr/wp-content/uploads/2023/02/groupon-1-scaled.jpg.webp",
 	prix:"70€"
 },
 	{
+	id:"4",
 	nom:"Bouquet fête des grands-mères",
 	img:"https://kokomo.fr/wp-content/uploads/2023/02/groupon-1-scaled.jpg.webp",
 	prix:"70€"
 },
 	{
+	id:"5",
 	nom:"Bouquet fête des grands-mères",
 	img:"https://kokomo.fr/wp-content/uploads/2023/02/groupon-1-scaled.jpg.webp",
 	prix:"70€"
 },
 	{
+	id:"6",
 	nom:"Bouquet fête des grands-mères",
 	img:"https://kokomo.fr/wp-content/uploads/2023/02/groupon-1-scaled.jpg.webp",
 	prix:"70€"
@@ -41,6 +47,7 @@ const HomePage = () =>{
 		{products.map((produict, index)=>(
 	            <Articl 
 			key = {index}
+			id= {produict.id}
 			img = {produict.img}
 			nom = {produict.nom}
 			prix = {produict.prix}
