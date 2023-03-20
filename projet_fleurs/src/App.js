4f6e874847d10a31975b9de2a5fb45e33eaae7fc
Dre-Drey <51761267+Dre-Drey@users.noreply.github.com>
2023-03-20 16:22:35 +0100

    mise en page homepage + produit

--- projet_fleurs/src/App.js
+++ projet_fleurs/src/App.js
@@ -1,15 +1,15 @@
-import { Routes, Route } from 'react-router-dom';
-import Header from './components/Header/Header';
-import HomePage from './components/Home/HomePage';
-import ProductPage from './components/Product/ProductPage';
-import BasketPage from './components/Basket/BasketPage';
-import InscriptionPage from './components/Inscription/InscriptionPage';
-import ConnectionPage from './components/Connection/ConnectionPage';
-import Footer from './components/Footer/Footer';
-import User from './components/User/UserPage';
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import HomePage from "./components/Home/HomePage";
+import ProductPage from "./components/Product/ProductPage";
+import BasketPage from "./components/Basket/BasketPage";
+import InscriptionPage from "./components/Inscription/InscriptionPage";
+import ConnectionPage from "./components/Connection/ConnectionPage";
+import Footer from "./components/Footer/Footer";
+import User from "./components/User/UserPage";
 function App() {
   return (
-    <div>
+    <div className="body-font font-Josefin">
       <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
--- projet_fleurs/src/components/Product/Article-components/ArticlePhoto.jsx
+++ projet_fleurs/src/components/Product/Article-components/ArticlePhoto.jsx
@@ -1,42 +1,46 @@
-import React, {useState} from "react";
-import photo1 from "../../../img/ficus-1.jpg";
-import photo2 from "../../../img/ficus-2.jpg";
-import photo3 from "../../../img/ficus-3.jpg";
+import React, { useState } from "react";
 
 const ArticlePhoto = (props) => {
+  // récupérer les images
+  const [sliderData, setSliderData] = useState(props.images);
 
-  // récouperer images
-  const[sliderData, setSliderData] = useState(props.images);
-  
-  // par quel condition afficher les images
-  const handleClick=(index)=>{
-    const slider=props.images[index];
+  // à quelles conditions afficher les images
+  const handleClick = (index) => {
+    const slider = props.images[index];
     setSliderData(slider);
-  }
-  
+  };
+
   return (
     <>
       <div id="photos" className="w-1/2 flex justify-center flex-col">
-        
         <div className="w-full flex justify-center">
-          
-          <img  className="w-[620px] h-[370px]" src={sliderData} alt="images" />
+          <img
+            className="w-[620px] max-h-full object-cover"
+            src={sliderData}
+            alt="images"
+          />
         </div>
-        <div id="display-all-photos" className=" mt-[-45px] mb-[40px] w-full flex justify-center">
+        <div
+          id="display-all-photos"
+          className=" mt-[-45px] mb-[40px] w-full flex justify-center"
+        >
           {
             // porps.images.map((data, i)=>
-            props.images.map((image, i) =>
-              <div className=" w-[21%] flex justify-center">
-                <img className="w-[138px] h-[115px]" key={i} src={image} onClick={()=>handleClick(i)} alt="images" />
-
+            props.images.map((image, i) => (
+              <div className=" w-[21%] mx-10 flex justify-center">
+                <img
+                  className="w-[138px] h-[115px]"
+                  key={i}
+                  src={image}
+                  onClick={() => handleClick(i)}
+                  alt="images"
+                />
               </div>
-              )
+            ))
           }
-
         </div>
-
       </div>
-      
+
       {/* <div id="photos" className="w-1/2 border">
         <div if="main-photo" className="mr-20 ml-20">
           <img src={photo1} alt="main photo" className="w-auto border"></img>
