0d8e99c93c1152e4ee1b3dc12b89845aa61154de
HeleneVeber <veber.helene@gmail.com>
2023-03-22 11:35:40 +0100

    passe useState loggin to nav

--- projet_fleurs/src/components/Header/Header.jsx
+++ projet_fleurs/src/components/Header/Header.jsx
@@ -1,18 +1,15 @@
 import React from "react";
-
-import Logo from "../../img/logo_720.jpg";
-import PanierIcon from "../../img/panier.png";
-import LoginIcon from "../../img/login.png";
-import { Link } from "react-router-dom";
 import Banner from "./HeaderComponent/Banner";
 import Nav from "./HeaderComponent/Nav";
 
-const Header = () => {
+const Header = ({ loggin, setLoggin }) => {
+  console.log("header", loggin);
+
   return (
     <>
       <Banner />
 
-      <Nav />
+      <Nav loggin={loggin} setLoggin={setLoggin} />
     </>
   );
 };
