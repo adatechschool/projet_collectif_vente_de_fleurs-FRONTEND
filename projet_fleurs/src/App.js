2977ab447a67c9309be642ec081cd23023ef1ead
HeleneVeber <veber.helene@gmail.com>
2023-03-22 11:36:49 +0100

    cree useState loggin passe to Header et PageConnexion

--- projet_fleurs/src/App.js
+++ projet_fleurs/src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import HomePage from "./components/Home/HomePage";
@@ -7,20 +8,23 @@ import InscriptionPage from "./components/Inscription/InscriptionPage";
 import ConnectionPage from "./components/Connection/ConnectionPage";
 import Footer from "./components/Footer/Footer";
 import User from "./components/User/UserPage";
-import Nav from "./components/Header/HeaderComponent/Nav";
-import Navbar from "./components/Header/HeaderComponent/Categories";
+//import Nav from "./components/Header/HeaderComponent/Nav";
+//import Navbar from "./components/Header/HeaderComponent/Categories";
 
 function App() {
+  const [loggin, setLoggin] = useState();
+  console.log(loggin);
+
   return (
     <div className="body-font font-Josefin">
-      <Header />
+      <Header loggin={loggin} setLoggin={setLoggin} />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/produit/:uid" element={<ProductPage />} />{" "}
         {/* TODO : changer "uid" par produit id */}
         <Route path="/panier/:id" element={<BasketPage />} />
         <Route path="/users/signup" element={<InscriptionPage />} />
-        <Route path="/users/signin" element={<ConnectionPage />} />
+        <Route path="/users/signin" element={<ConnectionPage loggin={loggin} setLoggin={setLoggin} />} />
         <Route path="/user/:id" element={<User />} />
       </Routes>
       <Footer />
